c012f00b3b148cde30b41420a2e47a60154da8b9
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-07-15 19:32:27 -0600

    Add new methods and tests to turn

--- src/Turn.js
+++ src/Turn.js
@@ -21,18 +21,41 @@ class Turn {
     this.currentSpin = data.wheel[value];
   }
 
-  buyVowel() {
+  buyVowel(chosenVowel) {
     // from an array of vowels, splice vowel that is chosen
     // take 100 out of the players bank account when vowel 
     // is chosen
+    const vowels = ['A', 'E', 'I', 'O', 'U'];
+
+    if (vowels.includes(chosenVowel)) {
+      let foundIndex = vowels.findIndex((letter) => letter === chosenVowel);
+      vowels.splice(foundIndex, 1);
+      Player.score -= 100;
+      return vowels;
+    } else {
+      return 'Sorry Muggle! That vowel has disappeared!'
+    }
+
   }
 
-  solvePuzzle() {
+  solvePuzzle(answerGiven) {
     // should be able to guess if string of the guess is the
     // same as the string input of the player
+    let puzzleAnswer = Object.values(data.puzzles)[0].puzzle_bank[0].correct_answer;
+    if (puzzleAnswer === answerGiven) {
+      Player.score += this.spinWheel(); 
+    }
   }
 
-  updateScore() {
+  updateScore(letter) {
+    let puzzleAnswer = Object.values(data.puzzles)[0].puzzle_bank[0].correct_answer;
+    let splitAnswer = puzzleAnswer.split('');
+
+    if (splitAnswer.includes(letter)) {
+      Player.score += this.spinWheel(); 
+    } else {
+      // next player
+    }
     // if letter guessed is correct, add amount of money
     // that the wheel outputed into bank
   }
@@ -40,11 +63,15 @@ class Turn {
   goBankrupt() {
     // if wheel lands on bankrupt, player account is back to 0
     // move to next player
+    if (this.spinWheel === 'Bankrupt') {
+      Player.score = 0;
+    }
   }
 
-  endTurn() {
+  looseTurn() {
     // if player chooses wrong letter or vowel
     // move to next player 
+
   }
 
   compareGuessAgainstSolution() {
