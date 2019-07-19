7d33e00772949ce87fe381e8ec187e51ee749d31
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-07-19 15:50:58 -0600

    Add images

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import $ from 'jquery';
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
+import './images/cool-crystal.png'
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 // import './images/turing-logo.png'
